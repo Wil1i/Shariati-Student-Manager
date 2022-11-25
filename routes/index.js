258ddad57d4830729508eefb5b706f3530c42ea8
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-11-25 20:58:07 +0330

    Post method added

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ Router.get("/", homePageController.get);
 
 const loginPageController = require("../controllers/loginPageController");
 Router.get("/login", loginPageController.get);
+Router.post("/login", loginPageController.post, loginPageController.loginSuccess)
 
 const registerPageController = require("../controllers/registerPageController");
 Router.get("/register", registerPageController.get);
+Router.post("/register", registerPageController.post);
 
 const studentsListPageController = require("../controllers/studentListPageController");
 Router.get("/students/list", studentsListPageController.get);
