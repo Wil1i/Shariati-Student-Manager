39c54f2f8f83c67e8089a4b815cd8e7fb397a775
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-11-25 20:58:42 +0330

    Post method added

--- controllers/loginPageController.js
+++ controllers/loginPageController.js
@@ -1,7 +1,25 @@
+const passport = require("passport");
+
 const get = (req, res) => {
-    res.render("login")
+    res.render("login", {
+        flash : req.flash(),
+        user : req.user
+    })
+}
+
+const post = (req, res) => passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+    session: true
+})
+
+const loginSuccess = (req, res) => {
+    res.redirect(req.session.redirectTo || "/")
+    delete req.session.redirectTo;
 }
 
 module.exports = {
-    get
+    get,
+    post,
+    loginSuccess
 }
--- controllers/registerPageController.js
+++ controllers/registerPageController.js
@@ -1,7 +1,44 @@
+const User = require("../models/User")
+
 const get = (req, res) => {
-    res.render("register")
+    res.render("register", {
+        flash : req.flash(),
+        user : req.user
+    })
+}
+
+const post = async (req, res) => {
+    const isUserExist = await User.findOne({
+        where : {
+            username : req.body.username.toLowerCase()
+        }
+    }) || false
+
+    if(isUserExist){
+        req.flash("danger", "نام کاربری غیر قابل استفاده است")
+        return res.redirect("/register")
+    }else{
+        if(req.body.phoneNumber && req.body.phoneNumber.length == 11){
+            await User.create({
+                username : req.body.username.toLowerCase(),
+                password : await User.encryptPassword(req.body.password),
+                realName : req.body.realName,
+                phoneNumber : req.body.phoneNumber,
+                userRank : "teacher"
+            })
+    
+            req.flash("success", "حساب کاربری مورد نظر با موفقیت ساخته شد.")
+        }else{
+            req.flash("danger", "در وارد کردن شماره تلفن دقت کنید. (۰۹۰۰۰)")
+            return res.redirect("/register")
+        }
+        
+    }
+
+    res.redirect("/login")
 }
 
 module.exports = {
-    get
+    get,
+    post
 }
