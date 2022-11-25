a3e143fdd287dcf4201346dd30a3231534d4e76e
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-11-25 21:54:13 +0330

    Some functions added

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -1,5 +1,15 @@
-const get = (req, res) => {
-    res.render("index")
+const Qeybat = require("../models/Qeybat")
+const toFarsiNumber = require("../helpers/toFarsiNumber")
+
+const get = async (req, res) => {
+    const qeybats = await Qeybat.findAll()
+
+    res.render("index", {
+        flash : req.flash(),
+        user : req.user,
+        qeybats,
+        toFarsiNumber
+    })
 }
 
 module.exports = {
